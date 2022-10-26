0f7524df17269b519bc3a9013f016aa976f587e0
jabir11757 <smjalsaba11757@gmail.com>
2022-10-26 23:23:11 +0600

    FAQ page created

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -3,7 +3,15 @@ import React from 'react';
 const FAQ = () => {
     return (
         <div>
-            This is FAQ
+            <h2 className='d-flex justify-content-center'>Don't shy to meet</h2>
+            <div className='mt-5 d-flex justify-content-center'>
+                <div className='w-50 d-flex justify-content-center'>
+                    <h3>Ki obostha? </h3>
+                </div>
+                <div className='w-50  d-flex justify-content-center'>
+                    <h3>hello</h3>
+                </div>
+            </div>
         </div>
     );
 };
